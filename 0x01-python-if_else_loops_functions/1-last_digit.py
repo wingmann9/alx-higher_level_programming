fd43068ab0e3103c96c903b81ad429149a4919e3
wingmann <ema_eno@zoho.com>
2023-01-04 04:04:48 +0000

    Print the last digit of a number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+if number < 0:
+    last_digit = -last_digit
+print(f'Last digit of {number} is {last_digit} and is ', end="")
+if last_digit == 0:
+    print(f'0')
+elif last_digit > 5:
+    print(f'greater than 5')
+else:
+    print(f'less than 6 and not 0')
