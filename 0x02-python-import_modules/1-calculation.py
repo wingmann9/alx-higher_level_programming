f1e2bb2162029e7f5659e8ea1906d1c02aed6417
wingmann <ema_eno@zoho.com>
2023-01-05 23:08:41 +0000

    Import a function and execute it

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# stop the function from command line import
+if __name__ == "__main__":
+    """Print the sum, difference, product and quotient of a and be"""
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
