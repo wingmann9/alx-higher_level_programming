e55c3fce719c12546042cd0c0db21349f6739fff
wingmann <ema_eno@zoho.com>
2023-01-06 04:04:45 +0000

    List the number and value of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Stop the script from command line import"""
+if __name__ == "__main__":
+    """Print tlhe number, list and value of arguments from command line"""
+    import sys
+
+    arg = len(sys.argv) - 1
+    if arg == 0:
+        print("0 arguments.")
+    elif arg == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(arg))
+
+    for i in range(arg):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
