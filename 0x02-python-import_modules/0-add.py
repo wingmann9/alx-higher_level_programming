5f3eab9a6ab680d548246e5b771d879e8894309e
wingmann <ema_eno@zoho.com>
2023-01-06 18:58:27 +0000

    Fix pycodestyle error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Stop module from command line import"""
-if __name__ == "__main__":
-    """import the function add(a, b) from add_0.py script"""
-    import sys
-    from add_0 import add
+"""import add function from add-0 filw"""
+import sys
+from add_0 import add
+
+
+def main():
 
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-"""Create the 0-import_add file"""
-filename = "0-import_add.py"
-myfile = open(filename, 'w')
-myfile.write('__import__("0-add")\n')
-myfile.close()
+    sys.exit()
 
-sys.exit()
 
+"""stop script from CLI"""
+if __name__ == "__main__":
+    main()
