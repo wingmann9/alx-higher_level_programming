c9451e5a29da1ae69ee324ed65bca8fd149b9278
wingmann <ema_eno@zoho.com>
2023-01-06 04:09:02 +0000

    Program that imports a variable and print its value

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Stop the script from CLI"""
+if __name__ == "__main__":
+    """Import the varable a from variable_load_5.py"""
+    from variable_load_5 import a
+
+    """Print the value of a"""
+    print(a)
