07018aa0c535585eede88820901fb860932804ce
wingmann <ema_eno@zoho.com>
2023-01-06 05:19:48 +0000

    Fix error by creating 0-import_add file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,18 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/python3
 """Stop module from command line import"""
 if __name__ == "__main__":
     """import the function add(a, b) from add_0.py script"""
     import sys
-    import add_0
+    from add_0 import add
 
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
-    sys.exit()
+    print("{} + {} = {}".format(a, b, add(a, b)))
+"""Create the 0-import_add file"""
+filename = "0-import_add.py"
+myfile = open(filename, 'w')
+myfile.write('__import__("0-add")\n')
+myfile.close()
+
+sys.exit()
 
