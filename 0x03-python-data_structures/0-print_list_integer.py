56603347b39c596974865f9fea216537e0fcf70d
wingmann <ema_eno@zoho.com>
2023-01-09 16:08:15 +0000

    Fix pycodestyle error

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -6,8 +6,3 @@ def print_list_integer(my_list=[]):
     # initiate a for loop to print each value in the list
     for i in my_list:
         print("{}".format(i))
-
-
-# Stop the script from CLI
-if __name__ == "__main__":
-    print_list_integer()
