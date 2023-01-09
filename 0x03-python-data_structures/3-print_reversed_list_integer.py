3af6e8b7ebee69621d67515d5297c7469a12a838
wingmann <ema_eno@zoho.com>
2023-01-09 20:24:07 +0000

    Fix code error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
-# Function that print in reverse order, elements in a list
+# Function prints in reverse order, elements in a list
 def print_reversed_list_integer(my_list=[]):
 
-    # reverse list and iterate each element
-    for i in reversed(my_list):
-        print("{:d}".format(i))
-
-
-# Stop script from CLI
-if __name__ == "__main__":
-    print_reversed_list_integer()
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
