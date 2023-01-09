1f4aecbdd2eb8658ee44e01ccf108e547fef1e8f
wingmann <ema_eno@zoho.com>
2023-01-09 16:59:07 +0000

    Print a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Function to print a matrix of integers
+def print_matrix_integer(matrix=[[]]):
+
+    # iterate each element in the matrix
+    for i in matrix:
+        # convert and combine each element into a string
+        j = (" ".join(map(str, i)))
+        print("{}".format(j))
+
+
+# Stop script from CLI
+if __name__ == "__main__":
+    print_matrix_integer()
