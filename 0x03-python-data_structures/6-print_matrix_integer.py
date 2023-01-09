ddfafc0bce9f059bcd95bed86ac905efd27dea50
wingmann <ema_eno@zoho.com>
2023-01-09 20:30:35 +0000

    Fix requirement error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,13 +3,9 @@
 # Function to print a matrix of integers
 def print_matrix_integer(matrix=[[]]):
 
-    # iterate each element in the matrix
-    for i in matrix:
-        # convert and combine each element into a string
-        j = (" ".join(map(str, i)))
-        print("{:s}".format(j))
-
-
-# Stop script from CLI
-if __name__ == "__main__":
-    print_matrix_integer()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+        print("")
