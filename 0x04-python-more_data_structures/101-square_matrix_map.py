50b8851d25cebd6cd10014e2d3032575c329cd4e
wingmann <ema_eno@zoho.com>
2023-01-12 18:33:29 +0000

    Fix requirement error

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-# Function computes square of integers of a matrix using map()
 def square_matrix_map(matrix=[]):
-
-    # Using list(), map() & lambda() to return square of matrix
     return (list(map(lambda i: list(map(lambda x: x**2, i[:])), matrix)))
