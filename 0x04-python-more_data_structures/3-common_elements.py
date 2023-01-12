40c5a354696ab5c35938a15e1362069349c63414
wingmann <ema_eno@zoho.com>
2023-01-12 17:58:11 +0000

    Display common elements in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Function shows common elements in two sets
+def common_elements(set_1, set_2):
+
+    # return element common to set_1 & set_2 - set()
+    return (set_1 & set_2)
