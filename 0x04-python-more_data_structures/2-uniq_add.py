e3740277ed902385a8f5204b4694a3eeab3c6a0d
wingmann <ema_eno@zoho.com>
2023-01-12 17:57:02 +0000

    Sum up integers unique to a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Function sum up all unique integers in a list
+def uniq_add(my_list=[]):
+
+    # return the summation using the sum() and set()
+    return (sum(set(my_list)))
