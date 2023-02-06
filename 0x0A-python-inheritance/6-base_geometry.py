0699337cb6c05cdf824b0ed107cfee4b70f9018d
wingmann <ema_eno@zoho.com>
2023-02-06 23:09:07 +0100

    Define class BaseGeometry with public instance attribute

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines a base geometry class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Represent base geometry"""
+
+    def area(self):
+        """
+        Define area of BaseGeometry
+        Raise Excpetion to not implement the given function
+        """
+        raise Exception("area() is not implemented")
