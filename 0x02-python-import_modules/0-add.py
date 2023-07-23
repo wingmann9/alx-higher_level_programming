3a5289bba29ea8ab6e38194635b31493d181be0a
wingmann <ema_eno@zoho.com>
2023-07-23 11:25:20 +0100

    Fixed error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
-def main(a, b):
-    """Print the sum of a and b to stdout"""
-    print('{} + {} = {}'.format(a, b, add(a,  b)))
-
-
 if __name__ == '__main__':
     from add_0 import add
     """Import add() from add_0 file"""
 
     a = 1
     b = 2
-    main(a, b)
+
+    """Print the sum of a and b to stdout"""   
+    print('{} + {} = {}'.format(a, b, add(a,  b)))
