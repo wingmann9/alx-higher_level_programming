9b26019f136917c1fd84ebf457a4bc6fa364a1e5
wingmann <ema_eno@zoho.com>
2023-07-23 11:41:42 +0100

    Make changes to import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
-    import importlib
-    add = importlib.import_module('add_0')
+
+    import add_0
     """Import add() from add_0 file"""
 
     a = 1
     b = 2
 
     """Print the sum of a and b to stdout"""
-    print('{} + {} = {}'.format(a, b, add.add(a,  b)))
+    print('{} + {} = {}'.format(a, b, add_0.add(a,  b)))
