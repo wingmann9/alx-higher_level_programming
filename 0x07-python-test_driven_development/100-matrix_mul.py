5f35581ca82acb026985335194391bc3b5c24bec
wingmann <ema_eno@zoho.com>
2023-09-20 09:55:47 +0100

    Fixed requirement error

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -22,37 +22,41 @@ def matrix_mul(m_a, m_b):
     Returns:
         A new matrix of m_a * m_b
     """
- 
-    if m_a == [] or m_a == [[]]:
-        raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
-        raise ValueError("m_b can't be empty")
+    
+    if (isinstance(m_a, list) and all(isinstance(row, list) for row in m_a) and all(isinstance(ele, (int, float)) for row in m_a for ele in row)) and (isinstance(m_b, list) and all(isinstance(row, list) for row in m_b) and all(isinstance(ele, (int, float)) for row in m_b for ele in row)):
+        
+        if m_a == [] or m_a == [[]]:
+            raise ValueError("m_a can't be empty")
+        if m_b == [] or m_b == [[]]:
+            raise ValueError("m_b can't be empty")
+       
+        if not all(len(row) == len(m_a[0]) for row in m_a):
+            raise TypeError("each row of m_a must be of the same size")
+        if not all(len(row) == len(m_b[0]) for row in m_b):
+            raise TypeError("each row of m_b must be of the same size")
 
-    if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
+        if len(m_a[0]) != len(m_b):
+            raise ValueError("m_a and m_b can't be multiplied")
+        
+        new_matrix = [[sum(a * b for a, b in zip(row, col))
+                      for col in zip(*m_b)] for row in m_a]
+        
+        return new_matrix
+    
     else:
-        if not all(isinstance(row, list) for row in m_a):
-            raise TypeError("m_a must be a list of lists")
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
-    else:
-        if not all(isinstance(row, list) for row in m_b):
-            raise TypeError("m_b must be a list of lists")
 
-    if not all(isinstance(ele, (int, float)) for row in m_a for ele in row):
-        raise TypeError("m_a should contain only integers or floats")
-    if not all(isinstance(ele, (int, float)) for row in m_b for ele in row):
-        raise TypeError("m_b should contain only integers or floats")
+        if not isinstance(m_a, list):
+            raise TypeError("m_a must be a list")
+        else:
+            if not all(isinstance(row, list) for row in m_a):
+                raise TypeError("m_a must be a list of lists")
+        if not isinstance(m_b, list):
+            raise TypeError("m_b must be a list")
+        else:
+            if not all(isinstance(row, list) for row in m_b):
+                raise TypeError("m_b must be a list of lists")
 
-    if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must be of the same size")
-    if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must be of the same size")
-
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
-
-    new_matrix = [[sum(a * b for a, b in zip(row, col))
-                  for col in zip(*m_b)] for row in m_a]
-
-    return new_matrix
+        if not all(isinstance(ele, (int, float)) for row in m_a for ele in row):
+            raise TypeError("m_a should contain only integers or floats")
+        if not all(isinstance(ele, (int, float)) for row in m_b for ele in row):
+            raise TypeError("m_b should contain only integers or floats")
