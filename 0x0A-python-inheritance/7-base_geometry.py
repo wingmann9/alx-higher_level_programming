2d849553c43bca82c61579be7fe8b163262bde96
wingmann <ema_eno@zoho.com>
2023-09-30 15:34:57 +0100

    Fixed code error

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -23,4 +23,4 @@ class BaseGeometry:
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{name} must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
