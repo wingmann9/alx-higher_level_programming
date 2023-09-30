fe6419107d41a146d16c04013f23c18444591c5f
wingmann <ema_eno@zoho.com>
2023-09-30 08:29:54 +0100

    Fixed code error

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
             width(int): Width of the rectangle
             height(int): Height of the rectangle
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
