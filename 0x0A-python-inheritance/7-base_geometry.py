d29339e02fbeb0754786536bf5cb2ba12d4b9e5d
wingmann <ema_eno@zoho.com>
2023-09-30 15:21:20 +0100

    Python class BaseGeometry that validates a public instance attribute

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Reprsent a geometry"""
+
+    def area(self):
+        """Function not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validate a function parameter as an integer
+
+        Args:
+            name (str): Name of the parameter
+            value (int): Parameter to validate
+        Raises:
+            TypeError: If value is not an integer
+            ValueError: If value is <= 0
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError("{name} must be greater than 0")
